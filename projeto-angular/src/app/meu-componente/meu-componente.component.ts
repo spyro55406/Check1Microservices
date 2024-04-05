37bd62932be3deceb749fa7a452afbd12234b9ff
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-04 21:29:47 -0300

    commit final checkpoint1

---
+++ projeto-angular/src/app/meu-componente/meu-componente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-meu-componente',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './meu-componente.component.html',
+  styleUrl: './meu-componente.component.css'
+})
+export class MeuComponenteComponent {
+  pessoa = {nome: "Guilherme", idade: 19,  interesses: ["programação", "viagem", "fotografia"]}
+}
